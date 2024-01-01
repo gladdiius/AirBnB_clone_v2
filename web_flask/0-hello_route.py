5c8912724e4b5bd706ae154ce5180ff9271d0195
elias_eshetu <eliaseshetu28@gmail.com>
2024-01-01 19:43:09 +0300

    0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
 # a flask app
-from flask import Flask
 
+from flask import Flask
 app = Flask(__name__)
 
-# Define a route for the home page
 @app.route('/', strict_slashes=False)
 def home():
     return 'Hello HBNB!'
 
 if __name__ == '__main__':
-    # Run the application on 0.0.0.0:5000
     app.run(host='0.0.0.0', port=5000)
-
